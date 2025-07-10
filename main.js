82e1565ce24d4a3dc34200d2cea2577039919045
360-ahemenjeho <ahemenjeho2312@outlook.com>
2025-07-10 07:09:23 +0100

    feat(core logic implementation)

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('testing....')
+document.querySelectorAll('.menu').forEach(menu => {
+  const triggerEl = menu.querySelector('.trigger');
+  triggerEl.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+});
